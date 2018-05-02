ae811ef85b090491915c710db09403b00c2ed7f0
dmeskhi <dmeskh@gmail.com>
2018-05-01 19:18:48 -0700

    upload

--- script.js
+++ script.js
@@ -8,10 +8,38 @@ var body = document.getElementById("gradient");
 
 //Create new function for color pick
 function setGradient() {
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value +")";
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " 
+	+ color2.value +")";
 	h3.textContent = body.style.background + ";";
 }
 
+//Pick and set Bkground colors 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
+
+//Match gradient colors
+color1.addEventListener("input", setGradient());
+color2.addEventListener("input", setGradient());
+
+//Create button for random color pick
+var button = document.createElement("button");
+button.appendChild(document.createTextNode("Select Random Color"));
+body.appendChild(button);
+
+//Random color selector
+button.addEventListener("click", getRandomColor);
+
+function randomColor() {
+	var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' 
+	+ (Math.floor(Math.random() * 256)) + ',' 
+	+ (Math.floor(Math.random() * 256)) + ')';
+  return color;
+}
+
+function getRandomColor() {
+	var rndColor1 = randomColor();
+	var rndColor2 = randomColor();
+	body.style.background = "linear-gradient(to right, " + rndColor1 + ", " 
+	+ rndColor2 +")";
+	h3.textContent = body.style.background + ";";
+}
