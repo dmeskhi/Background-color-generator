df31ff62989f0fe7a7f9e1570ca6c7cba0a7d4be
dmeskhi <dmeskh@gmail.com>
2018-05-01 18:04:39 -0700

    upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Select h3 tag
+var h3 = document.querySelector("h3");
+//Select input colors
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+//Get background
+var body = document.getElementById("gradient");
+
+//Create new function for color pick
+function setGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value +")";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
